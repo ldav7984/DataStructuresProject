64eae13005cbcf48930d52a68ad2e9e6a96130bc
Davis, Lauren <ldav7984@740s-pl-109.local>
2019-01-28 12:11:07 -0700

    Initial Commit

---
+++ DataStructuresProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataStructuresProject
+//
+//  Created by Davis, Lauren on 1/28/19.
+//  Copyright © 2019 Davis, Lauren. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
