7721cd679ac2d5d0c6db3460f72fe213e5e6eacb
Davis <ldav7984@740s-pl-109.local>
2019-02-07 12:16:30 -0700

    Added a new List class to the model.

---
+++ DataStructuresProject/Model/Linear/List.hpp
@@ -0,0 +1,32 @@
+//
+//  List.hpp
+//  DataStructuresProject
+//
+//  Created by Davis, Lauren on 2/7/19.
+//  Copyright © 2019 Davis, Lauren. All rights reserved.
+//
+
+#ifndef List_h
+#define List_h
+
+#include "../Nodes/LinearNode.hpp"
+#include <assert.h>
+
+template <class> Type
+class List
+{
+protected:
+    int size;
+public:
+    //Structure
+    virtual void add(Type item) = 0;
+    virtual void addAtIndex(int index, Type item) = 0;
+    virtual Type remove(int index) = 0;
+    virtual Type getFromIndex(int index) = 0;
+    //Helper
+    virtual int getSize() const = 0;
+    virtual LinearNode<Type> * getFront() = 0;
+    virtual LinearNode<Type> * getEnd() = 0;
+};
+
+#endif /* List_h */
