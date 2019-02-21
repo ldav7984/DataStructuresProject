305dfad0f7ac39730177ae2a80418af14c1401b6
Davis <ldav7984@740s-pl-109.local>
2019-02-21 12:12:11 -0700

    Created a CircularList class in the linear folder.

---
+++ DataStructuresProject/Model/Linear/CircularList.hpp
@@ -0,0 +1,14 @@
+//
+//  CircularList.hpp
+//  DataStructuresProject
+//
+//  Created by Davis, Lauren on 2/21/19.
+//  Copyright © 2019 Davis, Lauren. All rights reserved.
+//
+
+#ifndef CircularList_hpp
+#define CircularList_hpp
+
+#include "../Nodes/DoubleNode.hpp"
+
+#endif /* CircularList_hpp */
