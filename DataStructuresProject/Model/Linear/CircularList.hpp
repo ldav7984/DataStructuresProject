d1d43c6e87071c3f2ca65fcfd55ffa9024ffb0f0
Davis <ldav7984@740s-pl-109.local>
2019-02-21 12:26:32 -0700

    Added class declaration and the constructor/destructor.

--- DataStructuresProject/Model/Linear/CircularList.hpp
+++ DataStructuresProject/Model/Linear/CircularList.hpp
@@ -11,4 +11,96 @@
 
 #include "../Nodes/DoubleNode.hpp"
 
+template <class Type>
+class CircularList : public DoubleNode<Type>
+{
+private:
+    DoubleNode<Type> * front;
+    DoubleNode<Type> * end;
+    int size;
+    
+    DoubleNode<Type> * findNode(int index);
+    
+public:
+    CircularList();
+    ~CircularList();
+    
+    //List methods
+    void add(Type item);
+    void addAtIndex(int index, Type item);
+    Type remove(int index);
+    Type setAtIndex(int index, Type item);
+    Type getFromIndex(int index);
+    int getSize() const;
+};
+
+//constructor
+template <class Type>
+CircularList<Type> :: CircularList()
+{
+    front = nullptr;
+    end = nullptr;
+    this->size = 0;
+}
+
+//destructor
+template <class Type>
+CircularList<Type> :: ~CircularList()
+{
+    DoubleNode<Type> * current = front;
+    while(this->front != nullptr ** this->front != front->getNext())
+    {
+        front = front->getNext();
+        delete current;
+        current = front;
+    }
+    delete front;
+}
+
+//Find Node
+template <class Type>
+DoubleNode<Type> * CircularList<Type> :: findNode(int index)
+{
+    assert (index >= 0 && index < this->size);
+    DoubleNode<Type> * nodeToFind;
+    
+    if(index < this->size / 2)
+    {
+        nodeToFind = this->front;
+        for(int position = 0; position < index; position++)
+        {
+            nodeToFind = nodeToFind->getNext();
+        }
+    }
+    else
+    {
+        nodeToFind = this->end;
+        for(int position = this->size - 1; position > index; position--)
+        {
+            nodeToFind = nodeToFind->getPrevious();
+        }
+    }
+    return nodeToFind;
+}
+
+//Add
+template <class Type>
+void CircularList<Type> :: add(Type item)
+{
+    DoubleNode<Type> * addedNode;
+    if(this->size == 0)
+    {
+        addedNode = new DoubleNode<Type(item);
+        this->front = addedNode;
+    }
+    else
+    {
+        addedNode = new DoubleNode<Type>(item, this->end, this-front);
+    }
+    this->end->setNext(addedNode);
+    this->front->setPrevious(addedNode);
+    this->end = addedNode;
+    this->size++;
+}
+
 #endif /* CircularList_hpp */
