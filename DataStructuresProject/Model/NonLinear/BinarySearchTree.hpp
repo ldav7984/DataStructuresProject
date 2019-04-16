51f82efc52db6a7322ca5d05c4170827929620c1
Davis <ldav7984@740s-pl-109.local>
2019-04-16 12:16:16 -0600

    Added protected and private definitions to binary search tree.

--- DataStructuresProject/Model/NonLinear/BinarySearchTree.hpp
+++ DataStructuresProject/Model/NonLinear/BinarySearchTree.hpp
@@ -9,5 +9,43 @@
 #ifndef BinarySearchTree_hpp
 #define BinarySearchTree_hpp
 
+#include "Tree.hpp"
+#include <iostream>
+
+using namespace std;
+
+template <class Type>
+class BinarySearchTree : public Tree<Type>
+{
+protected:
+    //MARK: Protected Methods
+    int caluclateSize(BinaryTreeNode<Type> * startNode);
+    int calculateHeight(BinaryTreeNode<Type> * startNode);
+    bool isBalanced(BinaryTreeNode<Type> * startNode);
+    bool isComplete(BinaryTreeNode<Type> * startNode, int index, int size);
+    
+    void inOrderTraversal(BinaryTreeNode<Type> * inStart);
+    void preOrderTraversal(BinaryTreeNode<Type> * preStart);
+    void postOrderTraversal(BinaryTreeNode<Type> * postStart);
+    
+    void destroyTree(BinaryTreeNode<Type> * node);
+    
+    BinaryTreeNode<Type> * getRightMostChild(BinaryTreeNode<Type> * current);
+    BinaryTreeNode<Type> * getLeftMostChild(BinaryTreeNode<Type> * current);
+    
+    void removeNode(BinaryTreeNode<Type> * removeMe);
+public:
+    //MARK: Public Methods
+    BinarySearchTree();
+    ~BinarySearchTree();
+    
+    BinaryTreeNode<Type> * getRoot();
+    
+    void inOrderTraveresal();
+    void preOrderTraversal();
+    void postOrderTraveresal();
+    void demoTraveresalSteps(BinaryTreeNode<Type> * node);
+    
+};
 
 #endif /* BinarySearchTree_hpp */
