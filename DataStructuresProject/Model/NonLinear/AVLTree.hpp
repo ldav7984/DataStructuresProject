34e897f569fbf2cf5dfc41d247e55ca2b9165c17
Davis <ldav7984@740s-pl-109.local>
2019-04-30 12:06:41 -0600

    Added the method to insert nodes to the AVL tree.

--- DataStructuresProject/Model/NonLinear/AVLTree.hpp
+++ DataStructuresProject/Model/NonLinear/AVLTree.hpp
@@ -16,12 +16,12 @@ class AVLTree : public BinarySearchTree<Type>
 {
 private:
     
-    BinaryTreeNode<Type> * leftRotation (BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * leftRotation(BinaryTreeNode<Type> * parent);
     BinaryTreeNode<Type> * rightRotation(BinaryTreeNode<Type> * parent);
     BinaryTreeNode<Type> * leftRightRotation(BinaryTreeNode<Type> * parent);
     BinaryTreeNode<TYpe> * rightLeftRotation(BinaryTreeNode<Type> * parent);
     
-    BinaryTreeNode<Type> * balanceSubTree (BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * balanceSubTree(BinaryTreeNode<Type> * parent);
     
     BinaryTreeNode<Type> * insertNode(BinaryTreeNode<Type> * parent, Type value);
     BinaryTreeNode<Type> * removeNode(BinaryTreeNode<Type> * parent, Type value);
@@ -34,8 +34,62 @@ public:
     void remove(Type value);
 };
 
+//Constructor
 template <class Type>
 AVLTree<Type> :: AVLTree() : BinarySearchTree<Type>()
+{
+    this->root = nullptr;
+}
+
+template <class Type>
+void AVLTree<Type> :: insert(Type item)
+{
+    insertNode(this->getRoot(), item); //recursive method
+}
+
+template <class Type>
+void AVLTree<Type> :: remove(Type item)
+{
+    removeNode(this->getRoot(), item); //recursive method
+}
+
+template <class Type> //recursion
+BinaryTreeNode<Type> * AVLTree<Type> :: insertNode(BinaryTreeNode<Type> * parent, Type inserted)
+{
+    if(parent == nullptr)
+    {
+        parent = new BinaryTreeNode<Type>(inserted);
+        
+        if(this->getRoot() == nullptr)
+        {
+            this->setRoot(parent);
+        }
+        return parent;
+    }
+    else if(inserted < parent->getNodeData())
+    {
+        parent->setLeftChild(insertNode(parent->getLeftChild(), inserted));
+        parent = balanceSubTree(parent);
+    }
+    else if(inserted > parent->getNodeData())
+    {
+        parent->setRightChild(insertNode(parent->getRightChild(), inserted));
+        parent = balanceSubTree(parent);
+    }
+    return parent;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * AVLTree<Type> :: removeNode(BinaryTreeNode<Type> * parent, Type inserted)
+{
+    
+}
+
+/*
+ 
+ */
+template <class Type>
+int AVLTree<Type> :: heightDifference(BinaryTreeNode<Type> * node)
 {
     
 }
